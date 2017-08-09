fd1c0b8a64d48c8507cdbc849648cd825b7e1769
Devops <devopsInabox@newtglobal.com>
2017-08-09 17:17:01 +0530

    Update predict.R

--- predict.R
+++ predict.R
@@ -1,5 +1,6 @@
 #install.packages("ggplot2",repos = "http://cran.us.r-project.org")
 #install.packages("gridExtra",repos = "http://cran.us.r-project.org")
+setwd("C:\\NewtDemo_Jenkins_workspace\\")
 options( warn = -1 )
 library(ggplot2)
 library(gridExtra)
